56da0085fc925a6d7379e3fdad9ac620f0b0b82d
Nathan Flick <nathanflick@gmail.com>
2015-03-21 17:51:35 -0700

    Added farthest points operation.

--- geospatial/src/main/java/asu/cse512/geospatial/Common.java
+++ geospatial/src/main/java/asu/cse512/geospatial/Common.java
@@ -4,6 +4,8 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.util.Arrays;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
@@ -27,9 +29,12 @@ public class Common {
 			System.out
 					.println("Usage: java -jar geospatial.jar <MASTER> <SPARK_HOME> <COMMAND> <ARG 1> [ARG 2] <OUTPUT>");
 			System.out
-					.println("\twhere COMMAND is one of { closest-points, farthest-points }");
+					.println("\twhere COMMAND is one of { closest-points, farthest-points, convex-hull }");
 			return;
 		}
+		
+		Logger log = Logger.getLogger("org");
+		log.setLevel(Level.WARN);
 
 		String master = args[0];
 		String sparkHome = args[1];
@@ -41,8 +46,7 @@ public class Common {
 				.getLocation().getPath();
 		String decodedPath = URLDecoder.decode(path, "UTF-8");
 
-		JavaSparkContext ctx = new JavaSparkContext(master, command, sparkHome,
-				new String[] { decodedPath });
+		JavaSparkContext ctx = new JavaSparkContext(master, command, sparkHome, decodedPath);
 
 		if (command.equals("closest-points")) {
 			String output = args[4];
@@ -52,6 +56,11 @@ public class Common {
 		} else if (command.equals("convex-hull")) {
 			String output = args[4];
 			Q2_ConvexHull.convexHull(ctx, input1, output, true);
+		} else if (command.equals("farthest-points")) {
+			String output = args[4];
+			JavaRDD<Point> points = readHDFSPointFile(ctx, input1);
+			PointPair farthest = FarthestPoints.farthestPoints(points);
+			writeHDFSPointPair(farthest, ctx, output);
 		}
 	}
 
