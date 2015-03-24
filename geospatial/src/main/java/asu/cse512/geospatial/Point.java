8922ccc15a0a636197041f8d984bf9f97c7016eb
Meng Yang <myang55@asu.edu>
2015-03-23 21:16:32 -0700

    add Junit test for convex hull

--- geospatial/src/main/java/asu/cse512/geospatial/Point.java
+++ geospatial/src/main/java/asu/cse512/geospatial/Point.java
@@ -3,45 +3,50 @@ package asu.cse512.geospatial;
 import java.io.Serializable;
 
 public class Point implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 
 	public Point(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	private final double x, y;
-	
+
 	public double getX() {
 		return x;
 	}
-	
+
 	public double getY() {
 		return y;
 	}
-	
+
 	public double distance(Point point) {
 		return Math.sqrt(Math.pow(x - point.x, 2) + Math.pow(y - point.y, 2));
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (obj == this) {
 			return true;
 		}
-		
+
 		if (!(obj instanceof Point)) {
 			return false;
 		}
-		
-		Point point = (Point)obj;
+
+		Point point = (Point) obj;
 		return point.x == x && point.y == y;
 	}
-	
+
 	@Override
 	public String toString() {
 		return Double.toString(x) + "," + Double.toString(y);
 	}
 
+	public boolean isSame(Point p) {
+		double epsilon = 0.000001;
+		return Math.abs((this.x - p.x)) < epsilon
+				&& Math.abs((this.y - p.y)) < epsilon;
+	}
 }
