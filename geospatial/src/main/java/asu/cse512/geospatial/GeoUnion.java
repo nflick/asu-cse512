a899029f0f21aa60ab2e331c6c813f18c730e4ac
Meng Yang <myang55@asu.edu>
2015-04-22 16:50:05 -0700

    rewrite union use union find, only union rectangles that are overlaped

--- geospatial/src/main/java/asu/cse512/geospatial/GeoUnion.java
+++ geospatial/src/main/java/asu/cse512/geospatial/GeoUnion.java
@@ -1,30 +1,52 @@
 package asu.cse512.geospatial;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.Iterator;
+import java.util.HashMap;
 import java.util.List;
-import java.util.ArrayList;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.*;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.broadcast.Broadcast;
 
+import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.io.ParseException;
 import com.vividsolutions.jts.io.WKTReader;
-import com.vividsolutions.jts.operation.union.*;
 
 public class GeoUnion {
 
-	// Parses a line from the inut file into a JTS Geometry object.
-	private static final Function<String, Geometry> POLYGON_EXTRACTOR = new Function<String, Geometry>() {
-		public Geometry call(String s) throws ParseException {
+	public static int[] unionSet;
+	public static HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+	public final static Function<String, Rectangle> RECTANGLE_EXTRACTOR = new Function<String, Rectangle>() {
+		private static final long serialVersionUID = 1L;
+
+		public Rectangle call(String s) {
+			// System.out.println("extractor 1");
 			List<String> array = Arrays.asList(s.split(","));
-			double x1 = Double.parseDouble(array.get(0));
-			double y1 = Double.parseDouble(array.get(1));
-			double x2 = Double.parseDouble(array.get(2));
-			double y2 = Double.parseDouble(array.get(3));
+			double x1 = 0.0, x2 = 0.0, y1 = 0.0, y2 = 0.0;
+			int id = -1;
+			x1 = Double.parseDouble(array.get(0));
+			y1 = Double.parseDouble(array.get(1));
+			x2 = Double.parseDouble(array.get(2));
+			y2 = Double.parseDouble(array.get(3));
+			Rectangle rectangle = new Rectangle(x1, y1, x2, y2);
+			return rectangle;
+		}
+	};
+	// Parses a line from the inut file into a JTS Geometry object.
+	private static final Function<Rectangle, Geometry> RECTANGLE_TO_JTS = new Function<Rectangle, Geometry>() {
+		public Geometry call(Rectangle r) throws ParseException {
+			double x1 = r.getX1();
+			double y1 = r.getY1();
+			double x2 = r.getX2();
+			double y2 = r.getY2();
 			String s2 = String.format(
 					"POLYGON ((%f %f, %f %f, %f %f, %f %f, %f %f))", x1, y1,
 					x2, y1, x2, y2, x1, y2, x1, y1);
@@ -39,49 +61,118 @@ public class GeoUnion {
 			return a.union(b);
 		}
 	};
+	private static final FlatMapFunction<Geometry, String> GEO_FORMATTER = new FlatMapFunction<Geometry, String>() {
 
-	// Allows us to perform a more efficient union by unioning an entire
-	// partition
-	// together at once. This uses the JTS CascadedPolygonUnion operation to do
-	// an efficient union on all of the Geometries in a single partition at the
-	// same time.
-	private static FlatMapFunction<Iterator<Geometry>, Geometry> PARTITION_MAP = new FlatMapFunction<Iterator<Geometry>, Geometry>() {
-		private static final long serialVersionUID = 1L;
-
-		public Iterable<Geometry> call(Iterator<Geometry> iter) {
-			ArrayList<Geometry> geometries = new ArrayList<Geometry>();
-			while (iter.hasNext()) {
-				geometries.add(iter.next());
+		public Iterable<String> call(Geometry g) throws Exception {
+			Coordinate[] cs = g.getCoordinates();
+			ArrayList<String> array = new ArrayList<String>();
+			for (int i = 0; i < cs.length - 1; i++) {
+				Coordinate c = cs[i];
+				array.add(c.x + "," + c.y);
 			}
-
-			ArrayList<Geometry> result = new ArrayList<Geometry>();
-			result.add(CascadedPolygonUnion.union(geometries));
-			return result;
+			return array;
 		}
+
 	};
 
 	// Main geometric union method.
 	public static void union(JavaSparkContext context, String input,
 			String output) {
+		long lStartTime = new Date().getTime();
 		JavaRDD<String> file = context.textFile(input);
 		// Map the input file to an RDD of Geometry objects.
-		JavaRDD<Geometry> rectangles = file.map(POLYGON_EXTRACTOR);
-		// Union partitions. The entire set of Geometry objects composing each
-		// partition
-		// will be unioned into a single Geometry. The end result will be a new
-		// RDD with
-		// a single Geometry object for each partition of Geometries that
-		// existed in the
-		// previous RDD>
-		JavaRDD<Geometry> partitionUnions = rectangles
-				.mapPartitions(PARTITION_MAP);
-		// Reduce these remaining geometries with JTS's union operation.
-		Geometry result = partitionUnions.reduce(REDUCER);
-		System.out.println(result);
-		Common.writeHDFSPoints(result, context, output);
-		context.close();
+		JavaRDD<Rectangle> rectangles = file.map(RECTANGLE_EXTRACTOR);
+		List<Rectangle> list = rectangles.collect();
+		int len = list.size();
+		unionSet = new int[len];
+
+		for (int i = 0; i < len; i++) {
+			unionSet[i] = i;
+		}
+		for (int i = 0; i < len; i++) {
+			Rectangle ri = list.get(i);
+			for (int j = i + 1; j < len; j++) {
+				if (ri.isOverlap(list.get(j))) {
+					unionxy(i, j);
+				}
+			}
+		}
+
+		for (int i = 0; i < len; i++) {
+			hashMap.put(list.get(i).getId(), unionSet[i]);
+		}
+		final Broadcast<HashMap<Integer, Integer>> broadcastHashMap = context
+				.broadcast(hashMap);
+		System.out.println("finish union find");
+		long lEndTime = new Date().getTime();
+		long difference = lEndTime - lStartTime;
+		System.out.println("running time: " + difference);
+		JavaRDD<Rectangle> rectangles2 = context.parallelize(list);
+		JavaPairRDD<Integer, Rectangle> tuples = rectangles2
+				.keyBy(new Function<Rectangle, Integer>() {
+
+					public Integer call(Rectangle v1) throws Exception {
+						// System.out.println("in keyby id= " + v1.getId());
+						return broadcastHashMap.value().get(v1.getId());
+					}
+
+				});
+		JavaPairRDD<Integer, Geometry> tuples2 = tuples.mapValues(
+				RECTANGLE_TO_JTS).reduceByKey(REDUCER);
+		JavaRDD<Geometry> polys = tuples2.values();
+		List<Geometry> list2 = polys.collect();
+		JavaRDD<String> result=polys.flatMap(GEO_FORMATTER);
+		
+		System.out.println("union result");
+		// for (Geometry g : list2)
+		// System.out.println(g);
+		result.saveAsTextFile(output);
+
 	}
 
+	public static int find(int x) {
+		int x2=x;
+		while (unionSet[x] != x) {
+			x = unionSet[x];
+		}
+		while (unionSet[x2] != x2){ 
+			int next=unionSet[x2];
+			unionSet[x2]=x;
+			x2=next;
+		}
+		return x;
+	}
 
+	public static void unionxy(int x, int y) {
+		int px = find(x);
+		int py = find(y);
+		unionSet[px] = py;
+	}
 
+	public static void main(String[] args) {
+		long lStartTime = new Date().getTime();
+		// String inputPath = "/home/steve/Documents/q1/input1.txt";
+		String inputPath = getFileN(10);
+		String outputFolder = "/home/steve/Documents/q1/output1";
+		JavaSparkContext context = getContext("Union");
+		union(context, inputPath, outputFolder);
+		long lEndTime = new Date().getTime();
+		long difference = lEndTime - lStartTime;
+		System.out.println("running time: " + difference);
+
+	}
+
+	// used for debug
+	public static JavaSparkContext getContext(String appName) {
+		SparkConf conf = new SparkConf().setAppName(appName).setMaster("local");
+		conf.set("spark.hadoop.validateOutputSpecs", "false");
+		JavaSparkContext context = new JavaSparkContext(conf);
+		return context;
+	}
+
+	public static String getFileN(int N) {
+		return String
+				.format("/mnt/hgfs/uBuntu_share_folder/arealm/arealm_reduced_%d.csv",
+						N);
+	}
 }
--- geospatial/src/main/java/asu/cse512/geospatial/Rectangle.java
+++ geospatial/src/main/java/asu/cse512/geospatial/Rectangle.java
@@ -2,13 +2,30 @@ package asu.cse512.geospatial;
 
 import java.io.Serializable;
 
-public class Rectangle implements Serializable{
+public class Rectangle implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private int id;
 	private double x1;
 	private double y1;
 	private double x2;
+
+	public double getX1() {
+		return x1;
+	}
+
+	public double getY1() {
+		return y1;
+	}
+
+	public double getX2() {
+		return x2;
+	}
+
+	public double getY2() {
+		return y2;
+	}
+
 	private double y2;
 
 	public String getIdToString() {
@@ -16,25 +33,35 @@ public class Rectangle implements Serializable{
 		return idString;
 	}
 
-	public Rectangle(int id,double x1,double y1,double x2,double y2){
-		this.id=id;
-		this.x1=Math.min(x1, x2);
-		this.y1=Math.max(y1, y2);
-		this.x2=Math.max(x1, x2);
-		this.y2=Math.min(y1, y2);
+	public Rectangle(int id, double x1, double y1, double x2, double y2) {
+		this.id = id;
+		this.x1 = Math.min(x1, x2);
+		this.y1 = Math.max(y1, y2);
+		this.x2 = Math.max(x1, x2);
+		this.y2 = Math.min(y1, y2);
+	}
+
+	public Rectangle(double x1, double y1, double x2, double y2) {
+		this.id = hashCode();
+		// System.out.println("in rec constructor id= "+id);
+		this.x1 = Math.min(x1, x2);
+		this.y1 = Math.max(y1, y2);
+		this.x2 = Math.max(x1, x2);
+		this.y2 = Math.min(y1, y2);
 	}
 
 	public boolean isIn(Rectangle small) {
 
-		if(small==null){
+		if (small == null) {
 			return false;
 		}
 		return x1 <= small.x1 && y1 >= small.y1 && x2 >= small.x2
 				&& y2 <= small.y2;
 	}
+
 	public boolean isPointIn(Point po) {
 
-		if(po==null){
+		if (po == null) {
 			return false;
 		}
 		return x1 <= po.getX() && y1 >= po.getY() && x2 >= po.getX()
@@ -43,10 +70,26 @@ public class Rectangle implements Serializable{
 
 	@Override
 	public String toString() {
-			return Integer.toString(id);
+		return Integer.toString(id);
 	}
-	
+
 	public int getId() {
 		return id;
+	}
+
+	public boolean isOverlap(Rectangle rec) {
+		boolean f1 = between(rec.x1, x1, x2) || between(rec.x2, x1, x2);
+		boolean f2 = f1 || between(x1, rec.x1, rec.x2)
+				|| between(x2, rec.x1, rec.x2);
+		boolean f3 = between(rec.y1, y1, y2) || between(rec.y2, y1, y2);
+		boolean f4 = f3 || between(y1, rec.y1, rec.y2)
+				|| between(y2, rec.y1, rec.y2);
+		return f2 && f4;
+	}
+
+	public static boolean between(double x, double a, double b) {
+		if (a <= b)
+			return a <= x && x <= b;
+		return b <= x && x <= a;
 	}
 }
